b9a5253ca31a07764299fec43c16619779348e2c
Brian Mulloy <brian@landlessness.net>
2024-04-30 11:53:04 -0400

    M3 shows the right peak which is good but also the old JT peak which is bad.

--- R/mami.codi.R
+++ R/mami.codi.R
@@ -76,18 +76,20 @@ duplex <- function(x, tolerance) {
   f = x$frequencies[[1]]
   λ = x$wavelengths[[1]]
 
+  highest_harmonic_num = round(max(f) / min(f))
+
   x %>% dplyr::mutate(
 
     # estimate the frequency cycle
     estimate_cycle(f,
-                   round(max(f) / min(f)),
+                   highest_harmonic_num,
                    x$pseudo_octave,
                    tolerance) %>%
       dplyr::rename_with(~ paste0(.,'_frequency')),
 
     # estimate the wavelength cycle
     estimate_cycle(λ,
-                   1 / round(max(λ) / min(λ)),
+                   1 / highest_harmonic_num,
                    x$pseudo_octave,
                    tolerance) %>%
       dplyr::rename_with(~ paste0(.,'_wavelength'))
