96cdb0cb0621af723969052c52304928a2df1560
Brian Mulloy <brian@landlessness.net>
2024-04-30 13:31:44 -0400

    no hamincs for freq

--- R/mami.codi.R
+++ R/mami.codi.R
@@ -81,7 +81,7 @@ duplex <- function(x, tolerance) {
 
     # estimate the frequency cycle
     estimate_cycle(f,
-                   n,
+                   1,
                    x$pseudo_octave,
                    tolerance) %>%
       dplyr::rename_with(~ paste0(.,'_frequency')),
