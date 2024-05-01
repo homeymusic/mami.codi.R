22a23bcf5ca88be6e9311112036e64f47a9900c4
Brian Mulloy <brian@landlessness.net>
2024-05-01 13:19:04 -0400

    done 4 partials. now trying octave of partials

--- R/mami.codi.R
+++ R/mami.codi.R
@@ -76,9 +76,10 @@ duplex <- function(x, tolerance) {
   f = x$frequencies[[1]]
   λ = x$wavelengths[[1]]
 
-  extra_partials = 4
+  extra_partials = 0
   n = x$pseudo_octave ^ log2(ceiling(2^(log(max(f) / min(f)) / log(x$pseudo_octave)))
                              + extra_partials)
+  n = x$pseudo_octave * n
 
   x %>% dplyr::mutate(
 
