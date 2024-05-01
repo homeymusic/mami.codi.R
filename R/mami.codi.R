c76d5f52b616ca3263d2e36a414fdc7210dffc8b
Brian Mulloy <brian@landlessness.net>
2024-04-30 22:26:41 -0400

    pseudo n

--- R/mami.codi.R
+++ R/mami.codi.R
@@ -75,20 +75,21 @@ duplex <- function(x, tolerance) {
 
   f = x$frequencies[[1]]
   λ = x$wavelengths[[1]]
-  pseudo_n = ceiling(x$pseudo_octave ^ log2(2 ^ (log(max(f) / min(f)) / log(x$pseudo_octave))))
+  n = ceiling(x$pseudo_octave ^ (log(max(f) / min(f)) / log(x$pseudo_octave)))
+
 
   x %>% dplyr::mutate(
 
     # estimate the frequency cycle
     estimate_cycle(f,
-                   max(f) / pseudo_n, # this is the lowest harmonic of the highest tone
+                   max(f) / n, # this is the lowest harmonic of the highest tone
                    x$pseudo_octave,
                    tolerance) %>%
       dplyr::rename_with(~ paste0(.,'_frequency')),
 
     # estimate the wavelength cycle
     estimate_cycle(λ,
-                   max(λ) / pseudo_n, # this is the highest harmonic of the lowest tone
+                   max(λ) / n, # this is the highest harmonic of the lowest tone
                    x$pseudo_octave,
                    tolerance) %>%
       dplyr::rename_with(~ paste0(.,'_wavelength'))
