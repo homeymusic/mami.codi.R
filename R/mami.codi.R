8213c3b1a7fe08851e9225390507204cfba9be2d
Brian Mulloy <brian@landlessness.net>
2024-04-30 22:18:08 -0400

    dropped the ceioling on e time

--- R/mami.codi.R
+++ R/mami.codi.R
@@ -75,7 +75,7 @@ duplex <- function(x, tolerance) {
 
   f = x$frequencies[[1]]
   λ = x$wavelengths[[1]]
-  pseudo_n = ceiling(x$pseudo_octave ^ log2(ceiling(2 ^ (log(max(f) / min(f)) / log(x$pseudo_octave)))))
+  pseudo_n = ceiling(x$pseudo_octave ^ log2(2 ^ (log(max(f) / min(f)) / log(x$pseudo_octave))))
   pseudo_n = max(f) / min(f)
 
   x %>% dplyr::mutate(
