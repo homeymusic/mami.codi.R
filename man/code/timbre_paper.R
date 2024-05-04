4f360fd9149f986d0c06b114c5e5178a5b4f1104
Brian Mulloy <brian@landlessness.net>
2024-05-04 10:03:52 -0400

    changed ref to main on readme data generators

--- man/code/timbre_paper.R
+++ man/code/timbre_paper.R
@@ -1,5 +1,5 @@
 source('./utils.R')
-devtools::install_github('git@github.com:homeymusic/mami.codi.R', ref='wave_freq_tol_relationship')
+devtools::install_github('git@github.com:homeymusic/mami.codi.R')
 
 library(mami.codi.R)
 devtools::load_all(".")
