e05b367eb1041b9b3a1543f829c6cc17aa725ffe
Brian Mulloy <brian@landlessness.net>
2024-05-03 21:52:57 -0400

    update;

--- man/code/roll_off_timbre_paper.R
+++ man/code/roll_off_timbre_paper.R
@@ -1,5 +1,5 @@
 source('./utils.R')
-devtools::install_github('git@github.com:homeymusic/mami.codi.R')
+devtools::install_github('git@github.com:homeymusic/mami.codi.R', ref='2D_tolerance')
 
 library(mami.codi.R)
 devtools::load_all(".")
