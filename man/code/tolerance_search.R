cab1684bd271c8f2f726db22bff1338ec66bd3c7
Brian Mulloy <brian@landlessness.net>
2024-05-18 09:40:03 -0400

    runs awith tol com stre

--- man/code/tolerance_search.R
+++ man/code/tolerance_search.R
@@ -1,19 +1,19 @@
 source('./trials.R')
 
-search_label = 'Bonang'
+search_label = 'Stretched'
 
 if (search_label == 'M3' || search_label == 'M6' || search_label == 'P8') {
   tolerances   = c(1 %o% 10^(-8:-1))
 } else {
 
   # Detailed
-  from_tol   = 0.04
-  to_tol     = 0.07
-  by_tol     = 0.001
-  tolerances = seq(from=from_tol, to=to_tol, by=by_tol)
+  # from_tol   = 0.04
+  # to_tol     = 0.07
+  # by_tol     = 0.001
+  # tolerances = seq(from=from_tol, to=to_tol, by=by_tol)
 
   # Orders of Magnitude
-  # tolerances   = c(1:9 %o% 10^(-4:-2))
+  tolerances   = c(1:9 %o% 10^(-4:-1))
 }
 
 run_trials(search_label, tolerances)
