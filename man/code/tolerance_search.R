ef8c354f27e37f2c19ba4bcce4dce085f9ed2548
Brian Mulloy <brian@landlessness.net>
2024-05-21 12:59:20 -0400

    hmm ver similar

--- man/code/tolerance_search.R
+++ man/code/tolerance_search.R
@@ -1,22 +1,24 @@
+devtools::install_github('git@github.com:homeymusic/mami.codi.R',
+                         ref='lcm_as_dissonance')
 source('./trials.R')
 
-search_label = 'M3'
+search_label = 'Harmonic'
 
 if (search_label == 'M3' || search_label == 'M6' || search_label == 'P8') {
   # Detailed
-  from_tol   = 4e-05
-  to_tol     = 6e-05
-  by_tol     = 1e-06
-  tolerances = seq(from=from_tol, to=to_tol, by=by_tol)
+  # from_tol   = 4e-05
+  # to_tol     = 6e-05
+  # by_tol     = 1e-06
+  # tolerances = seq(from=from_tol, to=to_tol, by=by_tol)
 
-  # tolerances   = c(1:9 %o% 10^(-6:-1))
+  tolerances   = c(1:9 %o% 10^(-6:-1))
 } else {
 
   # Detailed
-  from_tol   = 0.06
-  to_tol     = 0.08
-  by_tol     = 0.001
-  tolerances = seq(from=from_tol, to=to_tol, by=by_tol)
+  # from_tol   = 0.06
+  # to_tol     = 0.08
+  # by_tol     = 0.001
+  # tolerances = seq(from=from_tol, to=to_tol, by=by_tol)
 
   # Harmonic Best Results Range
   # low:  0.072
@@ -29,7 +31,7 @@ if (search_label == 'M3' || search_label == 'M6' || search_label == 'P8') {
   # high:
 
   # Orders of Magnitude
-  # tolerances   = c(1:9 %o% 10^(-4:-1))
+  tolerances   = c(1:9 %o% 10^(-4:-1))
 }
 
 run_trials(search_label, tolerances)
