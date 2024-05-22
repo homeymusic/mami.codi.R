f3e6c0ae7c134ec48958281edda8974a24a6c738
Brian Mulloy <brian@landlessness.net>
2024-05-22 19:33:30 -0400

    pure

--- man/code/precision_search.R
+++ man/code/precision_search.R
@@ -1,4 +1,4 @@
-search_label = 'Compressed'
+search_label = 'Pure'
 
 if (search_label == 'M3' || search_label == 'M6' || search_label == 'P8') {
   # Detailed
@@ -16,13 +16,13 @@ if (search_label == 'M3' || search_label == 'M6' || search_label == 'P8') {
   # by_prec     = 0.0001
   # precisions = seq(from=from_prec, to=to_prec, by=by_prec)
 
-  from_prec   = 0.03
-  to_prec     = 0.04
-  by_prec     = 0.001
-  precisions = seq(from=from_prec, to=to_prec, by=by_prec)
+  # from_prec   = 0.03
+  # to_prec     = 0.04
+  # by_prec     = 0.001
+  # precisions = seq(from=from_prec, to=to_prec, by=by_prec)
 
   # Orders of Magnitude
-  # precisions   = c(1:9 %o% 10^(-4:-1))
+  precisions   = c(1:9 %o% 10^(-4:-1))
 }
 
 devtools::install_github('git@github.com:homeymusic/mami.codi.R',
