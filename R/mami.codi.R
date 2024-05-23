71d13cc4ee1c6f253e105ca98b6e7fd899cf2d57
Brian Mulloy <brian@landlessness.net>
2024-05-23 06:52:26 -0400

    first

--- R/mami.codi.R
+++ R/mami.codi.R
@@ -74,17 +74,19 @@ parse_input.sparse_fr_spectrum <- function(x, ...) {
 compute_consonance = function(x, min_amplitude, spatial_precision, temporal_precision) {
 
   f       = x$spectrum[[1]] %>% dplyr::filter(.data$y>min_amplitude) %>% hrep::freq()
-  c_sound = max(f) / max(1/f)
-  l       = c_sound / f
+  P       = 1 / f
+  c_sound = max(P) / max(1/P)
+  l       = c_sound / P
 
   x %>% dplyr::mutate(
-    gcd(f/min(f), temporal_precision) %>% dplyr::rename_with(~ paste0('temporal_',.)),
+    gcd(P/min(P), temporal_precision) %>% dplyr::rename_with(~ paste0('temporal_',.)),
     temporal_consonance   = .data$temporal_gcd / 2,
     gcd(l/min(l), spatial_precision) %>% dplyr::rename_with(~ paste0('spatial_',.)),
     spatial_consonance    = .data$spatial_gcd  / 2,
     consonance_dissonance = .data$temporal_consonance + .data$spatial_consonance,
     major_minor           = .data$temporal_consonance - .data$spatial_consonance,
     frequencies           = list(f),
+    periods               = list(P),
     wavelengths           = list(l),
     speed_of_sound        = c_sound,
     min_amplitude,
