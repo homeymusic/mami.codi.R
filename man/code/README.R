66fe8109c84f5437961cf867da84a9cf51d8ee47
Brian Mulloy <brian@landlessness.net>
2024-05-23 09:55:35 -0400

    readme

--- man/code/README.R
+++ man/code/README.R
@@ -1,6 +1,5 @@
 source('./utils.R')
-devtools::install_github('git@github.com:homeymusic/mami.codi.R',
-                         ref='precision_1D')
+devtools::install_github('git@github.com:homeymusic/mami.codi.R')
 
 library(mami.codi.R)
 devtools::load_all(".")
