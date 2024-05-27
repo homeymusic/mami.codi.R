4b6aab2f20ae36c6fb76c3ed1729b8f778d51b88
Brian Mulloy <brian@landlessness.net>
2024-05-27 15:01:45 -0400

    about to serach

--- man/code/amplitude_search.R
+++ man/code/amplitude_search.R
@@ -1,13 +1,13 @@
 search_label = 'Rolloff12'
 
 # Detailed
-# from_amp   = 0.01
-# to_amp     = 0.1
-# by_amp     = 0.001
-# amplitudes = c(0,seq(from=from_amp, to=to_amp, by=by_amp))
+from_amp   = 0.01
+to_amp     = 0.9
+by_amp     = 0.01
+amplitudes = c(0,seq(from=from_amp, to=to_amp, by=by_amp))
 
 # Orders of Magnitude
-amplitudes   = c(0,c(1 %o% 10^(-3:0)))
+# amplitudes   = c(0,c(1 %o% 10^(-3:-1)))
 
 devtools::install_github('git@github.com:homeymusic/mami.codi.R')
 
