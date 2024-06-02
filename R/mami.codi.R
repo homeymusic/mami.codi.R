c572bf9df5030c7479ec22ca13bb44046940dc5d
Brian Mulloy <brian@landlessness.net>
2024-06-02 11:58:31 -0400

    fundamentals in thoughts looks good. need to put elsewhere

--- R/mami.codi.R
+++ R/mami.codi.R
@@ -126,11 +126,11 @@ compute_consonance = function(x, amplitude, precision, deviation) {
 
   x %>% dplyr::mutate(
 
-    alcd(f/min(f), precision, deviation, 'temporal'),
-    fundamental_frequency  = .data$temporal_consonance,
+    alcd(f/min(f), precision, deviation, 'frequency'),
+    fundamental_frequency  = min(f) / .data$frequency_alcd,
 
-    alcd(l/min(l), precision, deviation, 'spatial'),
-    fundamental_wavelength = .data$spatial_consonance,
+    alcd(l/min(l), precision, deviation, 'wavelength'),
+    fundamental_wavelength = min(l) / .data$wavelength_alcd,
 
     consonance_dissonance  = .data$fundamental_frequency + .data$fundamental_wavelength,
     major_minor            = .data$fundamental_frequency - .data$fundamental_wavelength,
@@ -179,7 +179,6 @@ alcd <- function(x, precision, deviation, label) {
   fractions = approximate_rational_fractions(x, precision, deviation)
   tibble::tibble_row(
     alcd        = lcm_integers(fractions$den),
-    consonance  = min(x) / .data$alcd,
     fractions   = list(fractions)
   ) %>% dplyr::rename_with(~ paste0(label, '_' , .))
 }
---
+++ man/thoughts/Fundamentals.md
@@ -0,0 +1,19 @@
+Fundamentals
+================
+
+## Fundamentals
+
+    #> # A tibble: 1 × 3
+    #>   `P1$fundamental_frequency` `P1$frequencies[[1]] %>% min` `P1$frequency_alcd`
+    #>                        <dbl>                         <dbl>               <dbl>
+    #> 1                       262.                          262.                   1
+
+    #> # A tibble: 1 × 3
+    #>   `P8$fundamental_frequency` `P8$frequencies[[1]] %>% min` `P8$frequency_alcd`
+    #>                        <dbl>                         <dbl>               <dbl>
+    #> 1                       262.                          262.                   1
+
+    #> # A tibble: 1 × 3
+    #>   `M3$fundamental_frequency` `M3$frequencies[[1]] %>% min` `M3$frequency_alcd`
+    #>                        <dbl>                         <dbl>               <dbl>
+    #> 1                       87.2                          262.                   3
