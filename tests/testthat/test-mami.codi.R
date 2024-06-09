49d038a53785b4f5ebc25684a2140cfd4222ff6b
Brian Mulloy <brian@landlessness.net>
2024-06-09 09:30:19 -0400

    1 10 an 20 harmonics

--- tests/testthat/test-mami.codi.R
+++ tests/testthat/test-mami.codi.R
@@ -3,6 +3,8 @@ test_that('P1 looks good',{
   expect_true(P1$consonance_dissonance > 0)
 })
 test_that('P1 pure tone looks good',{
-  P1 = c(60) %>% mami.codi(num_harmonics=1)
+  P1 = c(60) %>% mami.codi(num_harmonics=1, verbose=T)
   expect_true(P1$consonance_dissonance > 0)
+  expect_equal(P1$temporal_Sz, 0)
+  expect_equal(P1$spatial_Sz, 0)
 })
