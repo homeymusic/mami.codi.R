4447e83a66acb904c8299966b80b672b4cc34870
Brian Mulloy <brian@landlessness.net>
2024-10-29 12:36:57 -0400

    eadme

--- man/code/README.R
+++ man/code/README.R
@@ -1,6 +1,6 @@
 source('./utils.R')
 devtools::install_github('git@github.com:homeymusic/mami.codi.R',
-                         ref='plot_beats')
+                         ref='main')
 
 library(mami.codi.R)
 devtools::load_all(".")
