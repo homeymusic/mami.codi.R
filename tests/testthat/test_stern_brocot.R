14a57b6613930eac714167e77749c6931155ec26
Brian Mulloy <brian@landlessness.net>
2024-11-07 05:17:59 -0500

    test for SB unceratinty shortcut

--- tests/testthat/test_stern_brocot.R
+++ tests/testthat/test_stern_brocot.R
@@ -1,15 +1,26 @@
 test_that("stern brocot does not return 0", {
   x <- 0.1666667
-  uncertainty <- 0.200001
+  uncertainty <- 0.2
 
   result <- stern_brocot(x, uncertainty)
+  expect_equal(result, c(1,5))
   expect_false(result[1] == 0, 'Stern Brocot should never return a 0 value')
 })
 
 test_that("stern brocot does not return 0", {
-  x <- 0.1666667
   uncertainty <- 1/(4*pi)
+  x <- uncertainty  - 0.0001
 
   result <- stern_brocot(x, uncertainty)
+  expect_equal(result, c(1,12))
+  expect_false(result[1] == 0, 'Stern Brocot should never return a 0 value')
+})
+
+test_that("stern brocot does not return 0", {
+  uncertainty <- 1/(4*pi)
+  x <- uncertainty  + 0.0001
+
+  result <- stern_brocot(x, uncertainty)
+  expect_equal(result, c(1,7))
   expect_false(result[1] == 0, 'Stern Brocot should never return a 0 value')
 })
